e544d3d98c5d6dadb45ecac69a670591610c98ec
kolamiki <kolamiki@gmail.com>
2026-09-06 20:39:01 +0200

    chore: Files cleanup

--- test_resolve.mjs
+++
@@ -1,6 +0,0 @@
-import { readFileSync } from 'fs';
-const scenariosEnginePath = 'c:/Moje_Programy/e_motion/project/src/store/scenarioEngine.ts';
-const usersPath = 'c:/Moje_Programy/e_motion/project/src/mockData/users.json';
-const users = JSON.parse(readFileSync(usersPath, 'utf8'));
-
-console.log(users.allUsers.find(u => u.id === 'u_marinette'));
